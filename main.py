073070d9c18b152b8c29756eaabff53ab6b449d3
saadhanaa-srias <saadhanaasrias@gmail.com>
2024-09-17 19:08:00 +0530

    L11

--- main.py
+++ main.py
@@ -2,9 +2,8 @@ class Person:
     def __init__(self, fname, lname):
         self.firstname = fname
         self. lastname = lname
-
-def printname(self):
-    print(self.firstname, self.lastname)
+    def printname(self):
+        print(self.firstname, self.lastname)
 
 class Student(Person):
     def __init__(self, fname, lname, year):
